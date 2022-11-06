0a36965b0364e6b3bff18601f69a882b0889c170
IgnasBarakauskas <b.ignas05@gmail.com>
2022-11-06 20:56:34 +0100

    ec2 client support

--- src/shop/Client.java
+++ src/shop/Client.java
@@ -9,72 +9,87 @@ import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
 import software.amazon.awssdk.services.sns.model.PublishResponse;
 import software.amazon.awssdk.services.sns.model.SnsException;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
 
 public class Client {
 
 	public static void main(String[] args) {
 		// TODO
-		//get store name and filename from run args
+		// get store name and filename from run args
 		String storeName = args[0];
 		String date = args[1];
-		String fileName = date+"-"+storeName+".csv";
-		
-		//use store name and filename to load file to upload
-		File file = new File(ShopConstants.sales_files_folder+fileName); //file to upload
+		String fileName = date + "-" + storeName + ".csv";
+
+		// use store name and filename to load file to upload
+		File file = new File(ShopConstants.sales_files_folder + fileName); // file to upload
 
 		S3Client s3 = S3Client.builder().region(ShopConstants.region).build();
-		
+
 		try {
-			boolean bucketExists = BucketOperations.s3BucketExists(s3,ShopConstants.bucket_name);
+			boolean bucketExists = BucketOperations.s3BucketExists(s3, ShopConstants.bucket_name);
 			System.out.println("Bucket Exists:" + bucketExists);
-			
-			if(!bucketExists) {
-				//create bucket
-				boolean bucketCreated = BucketOperations.createS3Bucket(ShopConstants.bucket_name, s3, ShopConstants.region );
-				
-				if(bucketCreated) {
-					System.out.println("Bucket:"+ ShopConstants.bucket_name + " created");
+
+			if (!bucketExists) {
+				// create bucket
+				boolean bucketCreated = BucketOperations.createS3Bucket(ShopConstants.bucket_name, s3,
+						ShopConstants.region);
+
+				if (bucketCreated) {
+					System.out.println("Bucket:" + ShopConstants.bucket_name + " created");
 					bucketExists = true;
-				}else {
-					System.out.println("Bucket:"+ ShopConstants.bucket_name + " could not be created");
+				} else {
+					System.out.println("Bucket:" + ShopConstants.bucket_name + " could not be created");
 				}
 			}
-			
-			if(bucketExists) {
+
+			if (bucketExists) {
 				Path filePath = file.toPath();
-				
-				//upload file to bucket
-				boolean fileUploaded = BucketOperations.uploadFileToBucket(s3, ShopConstants.bucket_name, fileName, filePath);
-				
-				if(fileUploaded) {
+
+				// upload file to bucket
+				boolean fileUploaded = BucketOperations.uploadFileToBucket(s3, ShopConstants.bucket_name, fileName,
+						filePath);
+
+				if (fileUploaded) {
 					System.out.println("file uploaded");
-					
-					//send notification to worker application (Lambda or VM application)
-					//Lambda
-					
-					try {
-						SnsClient snsClient = SnsClient.builder().region(ShopConstants.region).build();
-						PublishRequest request = PublishRequest.builder().message(storeName+";"+ShopConstants.bucket_name + ";" + fileName).topicArn(ShopConstants.topicARN).build();
-						
-						PublishResponse snsResponse = snsClient.publish(request);
-						System.out.println(snsResponse.messageId() + " Notification sent to worker. Status is " + snsResponse.sdkHttpResponse().statusCode());
-					}catch (SnsException e) {
-						System.err.println(e.awsErrorDetails().errorCode());
-						System.exit(1);
+
+					if (args[2].toString().equals("lambda")) {
+						System.out.println("Lambda will be used");
+						try {
+							SnsClient snsClient = SnsClient.builder().region(ShopConstants.region).build();
+							PublishRequest request = PublishRequest.builder()
+									.message(storeName + ";" + ShopConstants.bucket_name + ";" + fileName)
+									.topicArn(ShopConstants.topicARN).build();
+							PublishResponse snsResponse = snsClient.publish(request);
+							System.out.println(snsResponse.messageId() + " Notification sent to worker. Status is "
+									+ snsResponse.sdkHttpResponse().statusCode());
+						} catch (SnsException e) {
+							System.err.println(e.awsErrorDetails().errorCode());
+							System.exit(1);
+						}
+					} else if (args[2].toString().equals("ec2")) {
+						System.out.println("EC2 will be used");
+						SqsClient sqsClient = SqsClient.builder().region(ShopConstants.region).build();
+						SendMessageRequest sendRequest = SendMessageRequest.builder().queueUrl(ShopConstants.queueURL)
+								.messageBody(ShopConstants.bucket_name + ";" + fileName).build();
+						SendMessageResponse sqsResponse = sqsClient.sendMessage(sendRequest);
+						System.out.println(sqsResponse.messageId() + " Message sent. Status is "
+								+ sqsResponse.sdkHttpResponse().statusCode());
 					}
-					
-				}else {
+				} else {
 					System.out.println("file upload failed");
 				}
+				// send notification to worker application (Lambda or VM application)
+				// Lambda
+				// Notification sent
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 		}
-		
+
 	}
-	
-	
 
 }
--- src/shop/ShopConstants.java
+++ src/shop/ShopConstants.java
@@ -3,20 +3,25 @@ package shop;
 import software.amazon.awssdk.regions.Region;
 
 public class ShopConstants {
-	
-	//EC2
+
+	// EC2
 	public static Region region = Region.US_EAST_1;
 	public static String amiId = "ami-05fa00d4c63e32376";
-	
-	//S3
-	public static String bucket_name = "mezbucket2";
-	
-	//SQS
-//	public static String topicARN = "arn:aws:sns:us-east-1:996997097668:MyFirstTopic";
-	public static String topicARN ="arn:aws:sns:us-east-1:996997097668:SalesAppTopic";
-	
-	public static String sales_files_folder = "C:\\Users\\admin\\Downloads\\sales-data\\";
-	
+
+	// S3
+//	public static String bucket_name = "mezbucket2";
+	public static String bucket_name = "itsmeitsabucket";
+
+	// SNS
+	public static String topicARN = "arn:aws:sns:us-east-1:057004900367:SalesAppTopic";
+//	public static String topicARN ="arn:aws:sns:us-east-1:996997097668:SalesAppTopic";
+
+	// SQS
+	public static String queueURL = "https://sqs.us-east-1.amazonaws.com/057004900367/salesAppQueue";
+
+//	public static String sales_files_folder = "C:\\Users\\admin\\Downloads\\sales-data\\";
+	public static String sales_files_folder = "C:\\University\\UJM\\2nd year\\Cloud\\tp\\data\\";
+
 	public static String STORE_1 = "store1";
 	public static String STORE_2 = "store2";
 	public static String STORE_3 = "store3";
@@ -27,6 +32,5 @@ public class ShopConstants {
 	public static String STORE_8 = "store8";
 	public static String STORE_9 = "store9";
 	public static String STORE_10 = "store10";
-
 
 }
